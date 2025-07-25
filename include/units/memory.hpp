2c14ffb2adace003e4b7e9e7a3f59c33f7b48bde
rogerflowey <rogerwangabc@163.com>
2025-07-25 15:46:20 +0800

    feat: update memory part, use channels

--- include/units/memory.hpp
+++ include/units/memory.hpp
@@ -1,67 +1,85 @@
 #pragma once
 #include "constants.hpp"
-#include "utils/buffered.hpp"
+#include "logger.hpp"
+#include "utils/bus.hpp"
 #include "utils/clock.hpp"
-#include "utils/logger/logger.hpp"
-#include "utils/uints.hpp"
+#include "utils/ints.hpp"
 
-enum MemoryRequestType{
-    READ,WRITE
+enum MemoryRequestType { READ, WRITE };
+
+struct MemoryRequest {
+  MemoryRequestType type;
+  bool is_signed;
+  RobIDType rob_id;
+  RegDataType address;
+  uint8_t size;
+  MemDataType data;
+
+  static MemoryRequest CreateReadRequest(RobIDType rob_id, RegDataType address, uint8_t size, bool is_signed) {
+    return MemoryRequest{READ, is_signed, rob_id, address, size, {}};
+  }
+
+  static MemoryRequest CreateWriteRequest(RobIDType rob_id, RegDataType address, uint8_t size, MemDataType data) {
+    return MemoryRequest{WRITE, false, rob_id, address, size, data};
+  }
 };
 
-struct MemoryRequest{
-    MemoryRequestType type;
-    RobIDType rob_id;
-    RegType address;
-    u_int8_t size;
-    MemDataType data;
+struct MemoryResponse {
+  RobIDType rob_id;
+  MemDataType data;
 };
 
-struct MemoryResponse{
-    RobIDType rob_id;
-    MemDataType data;
-};
+class Memory {
+  alignas(8) std::byte memory[MEMORY_SIZE]{};
+  int time_cnt;
+  MemoryRequest request;
+  Channel<MemoryRequest> request_c;
+  Channel<MemoryResponse> response_c;
 
-class Memory{
-    alignas(8) std::byte memory[MEMORY_SIZE]{};
-    int time_cnt;
-    Buffered<MemoryRequest> request;
-    Buffered<MemoryResponse> response;
 public:
-    Memory(){
-        Clock::getInstance().subscribe([this]{this->tick();});
-    }
+  Memory() {
+    Clock::getInstance().subscribe([this] { this->tick(); });
+  }
 
-    void req(MemoryRequest request) {
-        if (time_cnt == 0) {
-            this->request <= request;
-            time_cnt = 3;
+  Channel<MemoryRequest> &get_request_channel() { return request_c; }
+
+  Channel<MemoryResponse> &get_response_channel() { return response_c; }
+
+  void tick() {
+    if (time_cnt == 0) {
+      // idle, try to accept new request
+      auto new_request = request_c.receive();
+      if (new_request) {
+        request = *new_request;
+        time_cnt = 3;
+        return;
+      }
+    } else {
+      time_cnt--;
+      if (time_cnt == 0) {
+        // finished request, check the channel
+        if (!response_c.can_send())
+          return;
+
+        if (request.type == READ) {
+          auto value =
+              request.is_signed
+                  ? static_cast<MemDataType>(
+                        bytes_to_sint(&memory[request.address],
+                                      &memory[request.address + request.size]))
+                  : bytes_to_uint(&memory[request.address],
+                                  &memory[request.address + request.size]);
+          response_c.send(MemoryResponse{ request.rob_id, value});
+          logger.With("ROB_ID", request.rob_id)
+              .With("Value", value)
+              .Info("Memory read");
         } else {
-            throw logger().Error("Memory request in progress");
+          response_c.send(MemoryResponse{ request.rob_id, 0});
+          logger.With("ROB_ID", request.rob_id)
+              .With("Value", request.data)
+              .Info("Memory write");
         }
+      }
     }
-
-    bool idle(){
-        return time_cnt==0;
-    }
-
-    MemoryResponse get_response(){
-        return *response;
-    }
-    void consume(){
-        response <= MemoryResponse{};
-    }
-
-    void tick(){
-        if(time_cnt>0){
-            time_cnt--;
-            if(time_cnt==0){
-                if(request->type==READ){
-                    response <= MemoryResponse{request->rob_id, bytes_to_uint(&memory[request->address], &memory[request->address + request->size])};
-                } else{
-                    response <= MemoryResponse{request->rob_id, 0};
-                }
-            }
-        }
-    }
+  }
 };
---
+++ include/units/mob.hpp
@@ -0,0 +1,92 @@
+#pragma once
+
+#include "cdb.hpp"
+#include "constants.hpp"
+#include "logger.hpp"
+#include "units/memory.hpp"
+#include "utils/bus.hpp"
+#include "utils/clock.hpp"
+#include "utils/queue.hpp"
+
+struct MOBEntry {
+  MemoryRequest req;
+  bool ready;
+  bool committed;
+};
+
+class MemoryOrderBuffer {
+  queue<MOBEntry, LSB_SIZE> queue;
+  CommonDataBus &cdb;
+  Memory &mem;
+
+  Channel<std::pair<RobIDType, MemoryRequestType>> mark_c;
+  Channel<MemoryRequest> fill_c;
+
+public:
+  MemoryOrderBuffer(CommonDataBus &cdb, Memory &mem) : cdb(cdb), mem(mem) {
+    Clock::getInstance().subscribe([this]{ this->work(); });
+  }
+
+  auto get_response_channel() -> Channel<MemoryResponse> & {
+    return mem.get_response_channel();
+  }
+
+  void work() {
+    // phase 0: regular work
+    if (!queue.full()) {
+      auto result = mark_c.receive();
+      if (result) {
+        auto [rob_id, type] = result.value();
+        queue.push_back(MOBEntry{{type, false, rob_id, 0, 0, 0}, false, false});
+        logger.With("ROB_ID", rob_id)
+            .With("Type", type == MemoryRequestType::READ ? "READ" : "WRITE")
+            .Info("MOBEntry added");
+      }
+    }
+    auto result = fill_c.receive();
+    if (result) {
+      auto id = result.value().rob_id;
+      for (size_t i = 0; i < queue.size(); ++i) {
+        if (queue[i].req.rob_id == id) {
+          queue[i].req = result.value();
+          queue[i].ready = true;
+
+          logger.With("ROB_ID", queue[i].req.rob_id)
+              .With("Type", queue[i].req.type == MemoryRequestType::READ
+                                ? "READ"
+                                : "WRITE")
+              .With("Addr", queue[i].req.address)
+              .Info("MOBEntry updated");
+        }
+      }
+    }
+
+    // phase 1: check cdb for STORE commit
+    auto cdb_result = cdb.get();
+    if (cdb_result) {
+      for (size_t i = 0; i < queue.size(); ++i) {
+        if (queue[i].req.rob_id == cdb_result->rob_id) {
+          queue[i].committed = true;
+        }
+      }
+    }
+
+    // phase 2: send new memory request
+    if (!queue.empty()) {
+      MOBEntry entry = queue.front();
+      if (entry.req.type == READ || entry.committed) {
+        if (mem.get_request_channel().can_send()) {
+          mem.get_request_channel().send(entry.req);
+          queue.pop_front();
+
+          logger.With("ROB_ID", entry.req.rob_id)
+              .With("Type", entry.req.type == MemoryRequestType::READ
+                                ? "READ"
+                                : "WRITE")
+              .With("Addr", entry.req.address)
+              .Info("Sending memory request");
+        }
+      }
+    }
+  }
+};
