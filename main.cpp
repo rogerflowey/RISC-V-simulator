b1e6cfece788df2524c6b3296ce7f2e8f427ae60
rogerflowey <rogerwangabc@163.com>
2025-07-30 21:56:29 +0800

    fix:corrected interface

--- main.cpp
+++ main.cpp
@@ -7,12 +7,16 @@
 
 int main() {
     //std::ofstream log_file("cpu_sim.log");
+    //logger.SetStream(log_file);
     logger.SetStream(std::cerr);
+    //logger.SetLevel(LogLevel::INFO);
     logger.SetLevel(LogLevel::ERROR);
+    //std::ifstream data_file("../data/testcases/qsort.data");
     try {
-        auto byte_memory = Loader::parse_memory_image(std::cin);
-        auto instruction_memory = Loader::convert_bytes_to_words(byte_memory);
-        CPU cpu(instruction_memory);
+        //auto initial_memory_image = Loader::parse_memory_image(data_file);
+        auto initial_memory_image = Loader::parse_memory_image(std::cin);
+        CPU cpu(initial_memory_image);
+
         Clock& clock = Clock::getInstance();
         while (true) {
             clock.tick();
